67308cc01b3e1e6055ec60bd6312859e96cce04d
Daniel Otieno <danielotieno.ke@gmail.com>
2019-03-23 19:38:06 +0300

    Create database connection

--- btre/settings.py
+++ btre/settings.py
@@ -66,8 +66,11 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dannydb',
+        'USER': 'postgres',
+        'PASSWORD': 'Daniel1994',
+        'HOST': 'localhost'
     }
 }
 
