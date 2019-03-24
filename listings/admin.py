33595fa19acbec515acf3f47361a089bee238e48
Daniel Otieno <danielotieno.ke@gmail.com>
2019-03-24 14:57:50 +0300

    Customize listing data display on admin area

--- listings/admin.py
+++ listings/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 
 from .models import Listing
 
-admin.site.register(Listing)
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published',
+                    'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'address', 'description',
+                     'city', 'state', 'price', 'bedroom')
+    list_per_page = 25
+
+
+admin.site.register(Listing, ListingAdmin)
