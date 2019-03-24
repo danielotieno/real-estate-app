1f55191c5abb2611011b089133f98389b7412542
Daniel Otieno <danielotieno.ke@gmail.com>
2019-03-24 11:27:59 +0300

    Create Realtors model

--- listings/models.py
+++ listings/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-
 from datetime import datetime
+from realtors.models import Realtor
 
 
 class Listing(models.Model):
@@ -26,3 +26,6 @@ class Listing(models.Model):
     photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+
+    def __str__(self):
+        return self.title
