b0467c471868f7e8eff66fa7b71acee7eaf1d6ad
Daniel Otieno <danielotieno.ke@gmail.com>
2019-03-24 23:41:11 +0300

    Create dynamic pagination

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from .models import Listing
 
@@ -6,8 +7,12 @@ from .models import Listing
 def index(request):
     listings = Listing.objects.all()
 
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
     context = {
-        'listings': listings
+        'listings': paged_listings
     }
     return render(request, 'listings/listings.html', context)
 
