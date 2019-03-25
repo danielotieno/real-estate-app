6475122b462a7f7588920e28a434bc4e7e5e3a3a
Daniel Otieno <danielotieno.ke@gmail.com>
2019-03-25 21:34:16 +0300

    Able to login new user

--- accounts/views.py
+++ accounts/views.py
@@ -49,8 +49,19 @@ def register(request):
 
 def login(request):
     if request.method == 'POST':
-        pass
         # Login User
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid Credentials')
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html')
 
