010657e9c276c74699313f6c2b71f72c7ac222bc
Daniel Otieno <danielotieno.ke@gmail.com>
2019-03-26 02:21:15 +0300

    Able to submit an inquiry

--- contacts/admin.py
+++ contacts/admin.py
@@ -4,7 +4,8 @@ from .models import Contact
 
 
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'listing', 'email', 'contact_date')
+    list_display = ('id', 'name', 'listing', 'email',
+                    'message', 'contact_date')
     list_display_links = ('id', 'name')
     search_fields = ('name', 'email', 'listing')
     list_per_page = 25
--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from .models import Contact
 
 
 def contact(request):
-    pass
+    if request.method == 'POST':
+        listing_id = request.POST['listing_id']
+        listing = request.POST['listing']
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        user_id = request.POST['user_id']
+        realtor_email = request.POST['realtor_email']
+
+        contact = Contact(listing_id=listing_id, listing=listing,
+                          name=name, email=email, phone=phone, message=message, user_id=user_id)
+
+        contact.save()
+
+        messages.success(
+            request, 'Your request has been submitted, a realtor willget back to soon')
+
+        return redirect('/listings/'+listing_id)
