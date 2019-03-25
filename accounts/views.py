b255f7228bee3dd766777c91d212a454f93467ed
Daniel Otieno <danielotieno.ke@gmail.com>
2019-03-25 23:53:26 +0300

    Create logout form

--- accounts/views.py
+++ accounts/views.py
@@ -71,4 +71,7 @@ def dashboard(request):
 
 
 def logout(request):
-    return redirect('index')
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, 'You have logged out')
+        return redirect('index')
