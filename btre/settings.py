273658185889549e2ab591dcc3661751e78bc730
Daniel Otieno <danielotieno.ke@gmail.com>
2019-03-25 18:27:19 +0300

    Create accounts app

--- btre/settings.py
+++ btre/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
