4bee10db2d127d3f3c3bb8b21992412c1a24f7d9
Daniel Otieno <danielotieno.ke@gmail.com>
2019-03-26 02:40:05 +0300

    Check if user has already made an inquiry

--- contacts/views.py
+++ contacts/views.py
@@ -15,6 +15,17 @@ def contact(request):
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
 
+        # Check if user has made inquiry already
+
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(
+                listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(
+                    request, 'You have already made an inquiry for this listing')
+                return redirect('/listings/'+listing_id)
+
         contact = Contact(listing_id=listing_id, listing=listing,
                           name=name, email=email, phone=phone, message=message, user_id=user_id)
 
