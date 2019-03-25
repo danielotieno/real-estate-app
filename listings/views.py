0544670f717b8f74fffe1975903a5b32950e2a29
Daniel Otieno <danielotieno.ke@gmail.com>
2019-03-25 18:12:28 +0300

    Make search items remains on the search bar

--- listings/views.py
+++ listings/views.py
@@ -68,7 +68,8 @@ def search(request):
         'bedroom_choices': bedroom_choices,
         'price_choices': price_choices,
         'state_choices': state_choices,
-        'listings': queryset_list
+        'listings': queryset_list,
+        'values': request.GET
     }
 
     return render(request, 'listings/search.html', context)
