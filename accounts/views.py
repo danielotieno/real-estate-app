9806908003d57f808b9d93260016bcdf7aa377c6
Daniel Otieno <danielotieno.ke@gmail.com>
2019-03-25 20:51:27 +0300

    Validate if passwords match

--- accounts/views.py
+++ accounts/views.py
@@ -4,9 +4,21 @@ from django.contrib import messages
 
 def register(request):
     if request.method == 'POST':
-        messages.error(request, 'Testing error messages')
-        return redirect('register')
-        # Register User
+        # Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # Check if passwords match
+
+        if password == password2:
+            pass
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
