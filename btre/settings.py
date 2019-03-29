0760c4b947b6156d9d31b85b2a572b6721978540
Daniel Otieno <danielotieno.ke@gmail.com>
2019-03-29 03:35:15 +0300

    Update settings file

--- btre/settings.py
+++ btre/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'do-realestate.herokuapp.com']
 
 
 # Application definition
